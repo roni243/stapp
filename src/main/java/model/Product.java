67d4c8388dccf326b544e2f476fcdac2c1a31398
getinthere <getinthere@naver.com>
2025-04-28 09:02:36 +0900

    테스트

---
+++ src/main/java/model/Product.java
@@ -0,0 +1,20 @@
+package model;
+
+import lombok.Builder;
+import lombok.Data;
+
+
+@Data
+public class Product {
+    private int id;
+    private String name; // 바지
+
+    @Builder
+    public Product(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
+
+
+

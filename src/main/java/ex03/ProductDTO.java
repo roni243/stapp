cca8d470c5d8ad60134516a514709ca68722b0da
roni <ksri2457@gmail.com>
2025-05-02 18:12:06 +0900

    00

---
+++ src/main/java/ex03/ProductDTO.java
@@ -0,0 +1,16 @@
+package ex03;
+
+import lombok.Data;
+import model.Product;
+
+@Data
+public class ProductDTO {
+    private int id;
+    private String name;
+
+    public ProductDTO(Product product) {
+        this.id = product.getId();
+        this.name = product.getName();
+    }
+}
+
